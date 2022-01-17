96ce84bfbc0f8ae49474d904c4ce954990b588fd
DerekRin <derekrin98@gmail.com>
2022-01-17 14:24:53 -0600

    Fixed issues and made a randomizer code

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,53 @@
 // Assignment code here
 //I understand that this won't be great, I'm getting a tutor monday to help explain these concepts more in depth and I plan to re-submit this at a later date. I apologize for my effort this time around. I promise to not dissapoint again.
 
-console.log("Would you like to generate a password?");
-console.log("Would you like lower case letters?");
-console.log("Would you like upper case letters?");
-console.log("Would you like to use numbers?");
-console.log("Would you like to use special characters?");
 
-var randomNumber = function(min, max) {
-  var value = Math.floor(Math.random() * (max - min) + min);
+var randomNumber = function(max) {
+  var value = Math.floor(Math.random() * max);
 
   return value;
 }
 
 function generatePassword() {
+    var password = "";
     var start = window.confirm("Would you like to generate a password?");
     var lower = window.confirm("Would you like to use lower case letters?");
       if (lower) {
-      criteria.push(lowercase);
-      String[lowercase] = new string[8];  
-      console.log(criteria);
-      return generatePassword;      
+      criteria = criteria + lowercase;
+    console.log(criteria.charAt(randomNumber(criteria.length)));     
     }
-    //var upper = window.confirm("Would you like to use upper case letters?");
-    //var numbers = window.confirm("Would you like to use numbers?");
-    //var special = window.confirm("Would you like to use special characters?");
 
-  }
+    var upper = window.confirm("Would you like to use upper case letters?");
+    if (upper) {
+    criteria = criteria + uppercase;
+    console.log(criteria.charAt(randomNumber(criteria.length)));
+    }   
 
-var criteria = [];
+    var numbers = window.confirm("Would you like to use numbers?");
+    if (numbers) {
+      criteria = criteria + numberic;
+      console.log(criteria.charAt(randomNumber(criteria.length)));
+    }
+
+    var special = window.confirm("Would you like to use special characters?");
+    if (special) {
+      criteria = criteria + specialchar;
+      console.log(criteria.charAt(randomNumber(criteria.length)));
+    }
+
+    for (var i = 0; i < 8; i++) {
+      password += criteria.charAt(randomNumber(criteria.length));
+      console.log(password);
+    }
+    return password;
+}
+
+var criteria = "";
 
 var lowercase = "abcdefghijklomnpqrstuvwxyz";
-//var uppercase = lowercase.toUpperCase();
-//var numberic = "0123456789";
-//var specialchar = "!@#$%&*";
+var uppercase = lowercase.toUpperCase();
+var numberic = "0123456789";
+var specialchar = "!@#$%&*";
 
 
 
