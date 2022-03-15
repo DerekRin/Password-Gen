e5cd5304cc8acf120f3c3a547bdbf3b43866d038
DerekRin <derekrin98@gmail.com>
2022-03-14 20:37:02 -0500

    WORKING 100%

---
+++ Develop/README.md
@@ -0,0 +1,13 @@
+# Password Generator Starter Code
+
+![alt text](https://github.com/DerekRin/Password-Gen/blob/main/Develop/images/website.png?raw=true)
+
+The following website is a password generator that will create the user a password using Javascript.
+
+![alt text](https://github.com/DerekRin/Password-Gen/blob/main/Develop/images/prompt.png?raw=true)
+
+By clicking the generator button the website will ask the user a series of questions to determine what kind of password they'd like. Such as if they want upper or lower case words, special characters, and numbers.
+
+![alt text](https://github.com/DerekRin/Password-Gen/blob/main/Develop/images/result.png?raw=true)
+
+After those questions are answered the user will get a password in the text box.
--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-// Assignment code here
-//I understand that this won't be great, I'm getting a tutor monday to help explain these concepts more in depth and I plan to re-submit this at a later date. I apologize for my effort this time around. I promise to not dissapoint again.
-
-
-var randomNumber = function(max) {
+var randomNumber = function (max) {
   var value = Math.floor(Math.random() * max);
 
   return value;
-}
+};
 
 function generatePassword() {
-    var password = "";
-    var start = window.confirm("Would you like to generate a password?");
-    var lower = window.confirm("Would you like to use lower case letters?");
-      if (lower) {
-      criteria = criteria + lowercase;
-    console.log(criteria.charAt(randomNumber(criteria.length)));     
-    }
+  var password = "";
+  criteria = "";
+  var start = window.confirm("Would you like to generate a password?");
+  if (start == false) {
+    return "Nothing";
+  }
 
-    var upper = window.confirm("Would you like to use upper case letters?");
-    if (upper) {
+  var lower = window.confirm("Would you like to use lower case letters?");
+  console.log("lower", lower);
+  if (lower) {
+    criteria = criteria + lowercase;
+    console.log(criteria.charAt(randomNumber(criteria.length)));
+  } else {
+    lower = false;
+  }
+
+  var upper = window.confirm("Would you like to use upper case letters?");
+  if (upper) {
     criteria = criteria + uppercase;
     console.log(criteria.charAt(randomNumber(criteria.length)));
-    }   
+  }
 
-    var numbers = window.confirm("Would you like to use numbers?");
-    if (numbers) {
-      criteria = criteria + numberic;
-      console.log(criteria.charAt(randomNumber(criteria.length)));
-    }
+  var numbers = window.confirm("Would you like to use numbers?");
+  if (numbers) {
+    criteria = criteria + numberic;
+    console.log(criteria.charAt(randomNumber(criteria.length)));
+  }
 
-    var special = window.confirm("Would you like to use special characters?");
-    if (special) {
-      criteria = criteria + specialchar;
-      console.log(criteria.charAt(randomNumber(criteria.length)));
-    }
+  var special = window.confirm("Would you like to use special characters?");
+  if (special) {
+    criteria = criteria + specialchar;
+    console.log(criteria.charAt(randomNumber(criteria.length)));
+  }
 
-    for (var i = 0; i < 8; i++) {
-      password += criteria.charAt(randomNumber(criteria.length));
-      console.log(password);
-    }
-    return password;
+  for (var i = 0; i < 8; i++) {
+    password += criteria.charAt(randomNumber(criteria.length));
+  }
+  return password;
 }
 
 var criteria = "";
 
 var lowercase = "abcdefghijklomnpqrstuvwxyz";
-var uppercase = lowercase.toUpperCase();
+var uppercase = "ABCDEFGHIJKLOMNPQRSTUVWXYZ";
 var numberic = "0123456789";
 var specialchar = "!@#$%&*";
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -61,7 +61,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button (DONE)
